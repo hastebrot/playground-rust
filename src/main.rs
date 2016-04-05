11f515258192ef91fb5da1529ecabd02d79fc553
Benjamin Gudehus <hastebrot@gmail.com>
2016-04-06 00:59:04 +0200

    (feat) Add simple ownership/borrowing example.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn same_length(s0: &String,
+               s1: &String) -> bool {
+    s0.len() == s1.len()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let source = "hello".to_owned();
+    let other = "hi".to_owned();
+    let other2 = "hola".to_owned();
+
+    println!("{:?}", same_length(&source, &other));
+    println!("{:?}", same_length(&source, &other2));
 }
